b0919f06da986ec31e4e1f162d3ca17563aff6f2
andrewc2020 <andrewc2020@gmail.com>
2018-11-05 14:56:25 +0000

    added cors directive

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-var http = require('http');
+const express = require('express')
+const app = express()
+const port = 4000
+var fs = require("fs"),
+    json;
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World! from CloudMagi");
-
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+app.get('/', (req, res) => res.json(
+    getConfig('products.json')
+));
 
-console.log("Server running at http://localhost:%d", port);
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+function readJsonFileSync(filepath, encoding){
+
+    if (typeof (encoding) == 'undefined'){
+        encoding = 'utf8';
+    }
+    var file = fs.readFileSync(filepath, encoding);
+    return JSON.parse(file);
+}
+
+function getConfig(file){
+
+    var filepath = './' + file;
+    return readJsonFileSync(filepath);
+}
